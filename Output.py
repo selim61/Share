0a30a5c92609d5467cc7686d1a113242db047b4f
Selim <selim.ozturk@student.hu.nl>
2018-09-19 15:43:55 +0200

    voor school

---
+++ Output.py
@@ -0,0 +1,6 @@
+geld = input("Wat verdien je per uur: ")
+uur = input("hoeveel uur heb je gewerkt: ")
+totaal = int(geld) * int(uur)
+print("%s uur werken levert %s Euro op" %(uur, totaal))
+
+
---
+++ Tuples.py
@@ -0,0 +1,7 @@
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+lijst = []
+lijst.append(letters.count("A"))
+lijst.append(letters.count("B"))
+lijst.append(letters.count("C"))
+
+print (lijst)
